9f9d8bb36f47e387790f2061247fa7e3178459d8
AJ Raftis <araftis@calpoly.edu>
2021-03-16 13:36:27 -0700

    Copied over some useful categories I created for my Cryptography and Computer Security class.

---
+++ Classes/Foundation/Extensions/BinaryInteger+Extensions.swift
@@ -0,0 +1,208 @@
+
+import Foundation
+
+public extension BinaryInteger {
+
+    var abs : Self {
+        if self < 0 {
+            return self * -1
+        } else {
+            return self
+        }
+    }
+
+    /**
+     Returns the GCD of receiver with value.
+     */
+    func gcd(_ right: Self) -> Self {
+        var a = self.abs
+        var b = right.abs
+
+        if b > a {
+            swap(&a, &b)
+        }
+
+        while b > 0 {
+            (a, b) = (b, a % b)
+        }
+
+        return a
+    }
+
+    /**
+     `true` if `n` is prime.
+     */
+    // Code: Adapted from https://www.geeksforgeeks.org/primitive-root-of-a-prime-number-n-modulo-n/
+    var isPrime : Bool {
+        // Corner cases
+        if self <= 1  {
+            return false
+        }
+        if self <= 3 {
+            return true
+        }
+
+        // This is checked so that we can skip middle five numbers in below loop
+        if self % 2 == 0 || self % 3 == 0 {
+            return false
+        }
+
+        var i : Int = 5
+        while i * i < self {
+            if Int(self) % i == 0 || Int(self) % (i + 2) == 0 {
+                return false
+            }
+            i += 6
+        }
+
+        return true
+    }
+
+    // Code: Adapted from https://www.geeksforgeeks.org/primitive-root-of-a-prime-number-n-modulo-n/
+    var primeFactors : Set<Self> {
+        var result = Set<Self>()
+        var n = self
+
+        // Print the number of 2s that divide n
+        while n % 2 == 0 {
+            result.insert(2)
+            n = n / 2
+        }
+
+        // n must be odd at this point. So we can skip one element (Note i = i +2)
+        var i = Self(3)
+        while i <= Self(sqrt(Float(n))) {
+            // While i divides n, print i and divide n
+            while n % Self(i) == 0 {
+                result.insert(i)
+                n = n / Self(i)
+            }
+            i += 2
+        }
+
+        // This condition is to handle the case when
+        // n is a prime number greater than 2
+        if n > 2 {
+            result.insert(n)
+        }
+
+        return result
+    }
+
+    // Code: Adapted from https://www.geeksforgeeks.org/primitive-root-of-a-prime-number-n-modulo-n/
+    /// Smallest primitive root of receiver
+    var smallestPrimitiveRoot : Self? {
+        // Check if n is prime or not
+        if !self.isPrime {
+            return nil
+        }
+
+        // Find value of Euler Totient function of n Since n is a prime number, the value of Euler  Totient function is n-1 as there are n-1  relatively prime numbers.
+        let phi = self - 1
+
+        // Find prime factors of phi and store in a set
+        let s = phi.primeFactors
+
+        // Check for every number from 2 to phi
+        for r in 2 ... Int(phi) {
+            // Iterate through all prime factors of phi and check if we found a power with value 1
+            var flag = false
+            for it in s {
+                // Check if r^((phi)/primefactors) mod n is 1 or not
+                if Self(r).raise(toPower: phi / it, mod: self) == 1 {
+                    flag = true
+                    break
+                }
+             }
+
+             // If there was no power with value 1.
+            if !flag {
+                return Self(r)
+            }
+        }
+
+        // If no primitive root found
+        return nil
+    }
+
+    /**
+     Compute the modular multiplicative inverse of receiver.
+
+     See: https://rosettacode.org/wiki/Modular_inverse#Swift
+     */
+    @inlinable
+    func modularInverse(_ mod: Self) -> Self {
+        var (m, n) = (mod, self)
+        var (x, y) = (Self(0), Self(1))
+
+        while n != 0 {
+            (x, y) = (y, x - (m / n) * y)
+            (m, n) = (n, m % n)
+        }
+
+        while x < 0 {
+            x += mod
+        }
+
+        return x
+    }
+
+    /**
+     Iterative Function to calculate (x^y)%p in O(log y)
+
+     While this can still take a while to compute on large numbers, it's much faster than computing (x^y) % p in the standard way.
+
+     This is compatible with `BigInt` should that ever make it into the base Swift distribution, so it's usable in cryptographic situations.
+
+      **Note:** *Adapted from [GeekForGeeks](https://www.geeksforgeeks.org/primitive-root-of-a-prime-number-n-modulo-n/) .*
+
+     - parameter y: Expoonent
+     - parameter p: Modulo
+
+     - returns:
+     */
+    func raise<T:BinaryInteger>(toPower y: T, mod p : T) -> T {
+        var result = T(1) // Initialize result
+        var xWork = self
+        var yWork = y
+
+        xWork = xWork % Self(p); // Update x if it is more than or equal to p
+
+        while (yWork > 0) {
+            // If y is odd, multiply x with result
+            if yWork & 1 != 0 {
+                result = (result * T(xWork)) % p
+            }
+
+            // y must be even now
+            yWork = yWork >> 1 // y = y/2
+            xWork = (xWork * xWork) % Self(p)
+        }
+        return result
+    }
+
+    func raise<T:BinaryInteger>(toPower y: T) -> T {
+        if y < 0 {
+            return T(0)
+        }
+
+        var result = T(1)
+        var base = T(self)
+        var exponent = y
+
+        repeat {
+            if exponent & 0x1 != 0 {
+                result *= base
+            }
+            exponent >>= 1
+            if exponent == 0 {
+                break
+            }
+            base *= base
+        } while true
+
+        return result
+    }
+
+}
+
---
+++ Classes/Foundation/Extensions/Data+Extensions.swift
@@ -0,0 +1,177 @@
+//
+//  Data+Extensions.swift
+//  AJRFoundation
+//
+//  Created by AJ Raftis on 3/16/21.
+//
+
+import Foundation
+
+public extension Data {
+
+    /**
+     Generates a test Data object where the values are (1 ... `length`) % 256, or values provided by your block.
+
+     Basically, this makes it easy to visually evaluate the results of a test.
+
+     - parameter length: The length of the data you want.
+     - parameter block: If present, the block will be called with an array of UInt8, which you can populate with whatever data you'd like.
+
+     - returns: The generated data object.
+     */
+    init(testDataOfLength length: Int, using block:((_ data: inout [UInt8]) -> Void)? = nil) {
+        var data = [UInt8](repeating: 0, count: length)
+        if let block = block {
+            block(&data)
+        } else {
+            for index in 0 ..< length {
+                data[index] = UInt8(index % 256)
+            }
+        }
+        self.init(bytes:data, count: length)
+    }
+
+    /**
+     Poor man's data dump, but somewhat useful for testing.
+     */
+    var dump : String {
+        var string = "(\(self.count))"
+        if self.count <= 16 {
+            string += " "
+        }
+        let digits = Int(ceil(log(Double(self.count)) / log(16)))
+        for index in self.startIndex ..< self.endIndex {
+            let hex = String(self[index], radix: 16)
+            if index % 16 == 0 && self.count > 16 {
+                if index + startIndex >= 16 {
+                    for subindex in index - 16 ..< index {
+                        if subindex < self.count + self.startIndex {
+                            var value = self[subindex]
+                            if value < 32 || (value > 0x7E && value < 0xA0) {
+                                value = 0x2E
+                            }
+                            string.append(Character(Unicode.Scalar(value)))
+                        }
+                    }
+                }
+                string += "\n\(String(format: "%0*x", digits, index)): "
+            }
+            string += (self[index] < 0x10 ? "0" : "") + hex + " "
+        }
+
+        let extra =  16 - (self.count - ((self.count / 16) * 16))
+        for _ in 0 ..< extra % 16 {
+            string.append("   ")
+        }
+        var startIndex = self.count - (16 - extra)
+        if startIndex == self.count && self.count > 16 {
+            startIndex -= 16
+        }
+        for index in startIndex ..< self.count {
+            var value = self[self.startIndex + index]
+            if value < 32 || (value > 0x7E && value < 0xA0) {
+                value = UInt8(46)
+            }
+            string.append(Character(Unicode.Scalar(value)))
+        }
+
+        return string
+    }
+
+    @inlinable
+    internal static func hexNibbleToInt(_ nibble: UInt8) -> UInt8? {
+        if nibble >= 65 && nibble <= 70 {
+            return nibble - 55
+        }
+        if nibble >= 97 && nibble <= 102 {
+            return nibble - 87
+        }
+        if nibble >= 48 && nibble <= 57 {
+            return nibble - 48
+        }
+        return nil
+    }
+
+    init?(hexString hex: String) {
+        if let ascii = hex.data(using: .ascii) {
+            let length = ascii.count
+            if length % 2 != 0 {
+                // We have to have two digits per
+                return nil
+            }
+            var buffer = [UInt8](repeating: 0, count: length / 2)
+
+            for index in stride(from: 0, to: length, by: 2) {
+                if let hiNibble = Data.hexNibbleToInt(ascii[index]),
+                   let loNibble = Data.hexNibbleToInt(ascii[index + 1]) {
+                    buffer[index / 2] = hiNibble * 16 + loNibble
+                } else {
+                    return nil
+                }
+            }
+            self.init(buffer)
+        } else {
+            return nil
+        }
+    }
+
+    static internal func nibble<T:BinaryInteger>(_ nibble: T) -> T? {
+        if nibble < 10 {
+            return nibble + 48
+        }
+        if nibble < 16 {
+            return nibble + 55
+        }
+        return nil
+    }
+
+    /**
+     A string representing the Data as hex numbers.
+     */
+    var hexString : String {
+        var raw = Data(repeating: 0, count: count * 2)
+
+        for (index, byte) in self.enumerated() {
+            raw[index * 2 + 0] = Data.nibble((byte & 0xF0) >> 4)!
+            raw[index * 2 + 1] = Data.nibble((byte & 0x0F) >> 0)!
+        }
+
+        return String(data: raw, encoding: .ascii)!
+    }
+
+    /**
+     Generates cryptographically secure random data using Apple's security framework.
+
+     - parameter length: How many bytes you want generated.
+
+     - returns: The random data.
+     */
+    init?(randomDataOfLength length: Int) {
+        var bytes = [UInt8](repeating: 0, count: length)
+
+        if SecRandomCopyBytes(kSecRandomDefault, length, &bytes) != 0 {
+            return nil
+        }
+
+        self.init(bytes)
+    }
+
+    mutating func size(toBits count: Int) -> Void {
+        let padCount = count % 8
+        let byteCount = count / 8 + (padCount == 0 ? 0 : 1)
+
+        self.count = byteCount
+
+        if padCount != 0 {
+            var padMask = UInt8(0)
+
+            for _ in 0 ..< padCount {
+                padMask <<= 1
+                padMask &= 0x1
+            }
+            self[byteCount - 1] &= padMask
+        }
+    }
+
+}
+
